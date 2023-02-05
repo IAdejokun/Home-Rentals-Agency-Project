13cded688d5ee7f6fe1d43d34ed4b6aad2ed3af7
IAdejokun <Adejokunibk@gmail.com>
2023-02-05 20:57:08 +0100

    Initial commit fot the home rentals project

---
+++ app.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([6.451140, 3.388400], 13);
+
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
